8b8bbeb4268a9731ecc91d967b387be6cff65d8e
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-03-04 14:07:28 +0300

    style(Main): change input icon style and add placeholder

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { ArrowRightIcon, Users2Icon } from 'lucide-vue-next';
-
+import { ArrowRightIcon, User2Icon } from 'lucide-vue-next';
 
 const userRef = ref('')
 </script>
@@ -17,20 +16,22 @@ const userRef = ref('')
         Введи пользователя и изучай его starred-репозитории с фильтрацией
       </p>
     </div>
-    <div
-      class="relative group">
-      <Users2Icon
-        class="absolute left-4 top-1/2 -translate-y-1/2 size-5 text-neutral-500 group-focus-within:text-accent duration-300 transition-colors" />
+    <div class="relative group">
+      <User2Icon
+        class="absolute left-4 top-1/2 -translate-y-1/2 size-5 text-neutral-500 group-focus-within:text-neutral-300 duration-300 transition-colors"
+      />
       <input
+        class="py-3.5 px-12 outline-2 outline-secondary focus:outline-accent rounded-2xl text-neutral-200 placeholder:text-neutral-600 duration-300 transition-all"
         v-model="userRef"
-        class="py-3.5 px-12 w-80 bg-secondary/20 outline-2 outline-secondary focus:outline-accent rounded-2xl text-neutral-200 placeholder:text-neutral-600 duration-300 transition-all"
-        type="text">
+        placeholder="Ввведите никнейм"
+        type="text"
+      >
       <div
-        class="absolute right-2.5 top-1/2 -translate-y-1/2  rounded-xl p-1.5 bg-secondary origin-right duration-150 transition-all"
-        :class="`${!userRef ? 'pointer-events-none' : 'pointer-events-auto'}`"
+        class="absolute cursor-pointer right-2.5 top-1/2 -translate-y-1/2  rounded-xl p-1.5 bg-secondary origin-right duration-150 transition-all"
+        :class="`${!userRef ? 'pointer-events-none translate-x-2' : 'pointer-events-auto translate-x-0'}`"
         :style="{scale: !userRef ? 0 : 1}"
       >
-        <ArrowRightIcon class="size-5" />
+        <ArrowRightIcon class="size-5 text-neutral-300" />
       </div>
     </div>
   </section>
