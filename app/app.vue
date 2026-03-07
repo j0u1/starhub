325a0ab7dc59047eba3ff78513a7cb3a2b0be8e9
j0u1 <158160207+j0u1@users.noreply.github.com>
2026-03-07 19:28:01 +0300

    feat: add seo and favicon

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,18 @@
+<script setup lang="ts">
+useSeoMeta({
+    title: "StarHub",
+    ogTitle: "StarHub",
+    description:
+        "StarHub платформа для просмотра звёзд конкретного пользователя на GitHub",
+    ogDescription:
+        "StarHub платформа для просмотра звёзд конкретного пользователя на GitHub",
+    twitterCard: "summary_large_image",
+});
+</script>
+
 <template>
-  <div class="min-h-screen flex flex-col bg-bg container mx-auto">
-    <NuxtRouteAnnouncer />
-    <NuxtPage />
-  </div>
+    <div class="min-h-screen flex flex-col bg-bg container mx-auto">
+        <NuxtRouteAnnouncer />
+        <NuxtPage />
+    </div>
 </template>
